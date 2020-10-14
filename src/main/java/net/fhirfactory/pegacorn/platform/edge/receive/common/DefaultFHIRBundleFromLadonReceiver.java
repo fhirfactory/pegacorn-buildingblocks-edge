5b497f1232e199e26367578df6c1d27b3937e6ea
Jasen Schremmer <jasen.schremmer@act.gov.au>
2020-10-14 13:07:53 +1100

    Added logging of all exceptions in the camel routes; Centralised edge endPoint code;

--- src/main/java/net/fhirfactory/pegacorn/platform/edge/receive/common/DefaultFHIRBundleFromLadonReceiver.java
+++ src/main/java/net/fhirfactory/pegacorn/platform/edge/receive/common/DefaultFHIRBundleFromLadonReceiver.java
@@ -1,17 +1,20 @@
 package net.fhirfactory.pegacorn.platform.edge.receive.common;
 
-import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.*;
-import net.fhirfactory.pegacorn.petasos.ipc.codecs.IPCPacketDecoderInitializerFactory;
-import net.fhirfactory.pegacorn.petasos.model.processingplant.DefaultWorkshopSetEnum;
-import net.fhirfactory.pegacorn.petasos.wup.archetypes.EdgeIngresMessagingGatewayWUP;
+import javax.inject.Inject;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.component.netty.ServerInitializerFactory;
 import org.apache.camel.spi.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
+import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.InterProcessingPlantHandoverPacketDecoderBean;
+import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.InterProcessingPlantHandoverRegistrationBean;
+import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.InterProcessingPlantHandoverResponseEncoderBean;
+import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.InterProcessingPlantHandoverResponseGenerationBean;
+import net.fhirfactory.pegacorn.petasos.ipc.beans.receiver.InterProcessingPlantHandoverUoWExtractionBean;
+import net.fhirfactory.pegacorn.petasos.ipc.codecs.IPCPacketDecoderInitializerFactory;
+import net.fhirfactory.pegacorn.petasos.model.processingplant.DefaultWorkshopSetEnum;
+import net.fhirfactory.pegacorn.petasos.wup.archetypes.EdgeIngresMessagingGatewayWUP;
 
 public abstract class DefaultFHIRBundleFromLadonReceiver extends EdgeIngresMessagingGatewayWUP {
 
@@ -20,11 +23,15 @@ public abstract class DefaultFHIRBundleFromLadonReceiver extends EdgeIngresMessa
 
     @Override
     protected void executePostInitialisationActivities(){
-        Registry registry = camelCTX.getRegistry();
-        ServerInitializerFactory ipcFromLandReceiverFactory = new IPCPacketDecoderInitializerFactory();
-        registry.bind("ipcFromLandReceiverFactory", ipcFromLandReceiverFactory);
+        executePostInitialisationActivities(camelCTX, specifyServerInitializerFactoryName());
     }
 
+    public static void executePostInitialisationActivities(CamelContext camelCTX, String serverInitializerFactoryName){
+        Registry registry = camelCTX.getRegistry();
+        ServerInitializerFactory serverInitializerFactory = new IPCPacketDecoderInitializerFactory();
+        registry.bind(serverInitializerFactoryName, serverInitializerFactory);
+    }
+    
     private String getWUPContinuityRoute() {
         return ("seda:" + this.getNameSet().getRouteCoreWUP() + ".InnerWUP.Continuity");
     }
@@ -42,7 +49,7 @@ public abstract class DefaultFHIRBundleFromLadonReceiver extends EdgeIngresMessa
         getLogger().debug("EdgeIPCReceiverWUPTemplate::configure(): hostname --> {}", this.getIngresTopologyEndpointElement().getHostname());
         getLogger().debug("EdgeIPCReceiverWUPTemplate::configure(): port --> {}", this.getIngresTopologyEndpointElement().getExposedPort());
 
-        from(this.ingresFeed())
+        fromWithStandardExceptionHandling(this.ingresFeed())
                 .routeId(getNameSet().getRouteCoreWUP())
                 .transform(simple("${bodyAs(String)}"))
                 .bean(InterProcessingPlantHandoverPacketDecoderBean.class, "handoverPacketDecode(*)")
@@ -51,29 +58,14 @@ public abstract class DefaultFHIRBundleFromLadonReceiver extends EdgeIngresMessa
                 .bean(InterProcessingPlantHandoverResponseGenerationBean.class, "generateInterProcessingPlantHandoverResponse(*,  Exchange," + this.getWupTopologyNodeElement().extractNodeKey() + ")")
                 .bean(InterProcessingPlantHandoverResponseEncoderBean.class, "responseEncoder(*)");
 
-        from(getWUPContinuityRoute())
+        fromWithStandardExceptionHandling(getWUPContinuityRoute())
                 .bean(InterProcessingPlantHandoverUoWExtractionBean.class, "extractUoW(*, Exchange)")
                 .to(egressFeed());
     }
 
     @Override
-    protected String specifyEndpointComponentDefinition() {
-        return ("netty");
-    }
-
-    @Override
-    protected String specifyEndpointProtocolLeadout() {
-        return ("?serverInitializerFactory=#ipcFromLandReceiverFactory&sync=true&disconnect=true&keepAlive=false");
-    }
-
-    @Override
-    protected String specifyEndpointProtocolLeadIn() {
-        return ("://");
-    }
-
-    @Override
-    protected String specifyEndpointProtocol() {
-        return ("tcp");
+    protected String specifyServerInitializerFactoryName() {
+        return "ipcFromLandReceiverFactory";
     }
 
     @Override
